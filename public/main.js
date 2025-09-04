95e7ea8450ad287e00d2433fec523ec825b92e1c
Daniel Mejia <dmejia@econtainers.co>
2025-09-04 07:50:36 -0500

    Implementamos funcionalidad de Sticky y Header traslucido

--- public/main.js
+++ public/main.js
@@ -30,3 +30,12 @@ eval("console.log('Gradiweb is online!!!');\n\n\n//# sourceURL=webpack://shopify
 /******/ 	
 /******/ })()
 ;
+
+document.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("header--scrolled");
+  } else {
+    header.classList.remove("header--scrolled");
+  }
+});
